8ffc2740567073f6cf0a938609b64fcd9fe5bde8
William <wmr_1014@yahoo.com>
2022-06-18 16:07:18 -0500

    new server members now added to stats file

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,18 +1,19 @@
 module.exports = {
     name: "guildMemberAdd",
     execute(member, stats) {
-        if (stats[member.user.id] !== null) {
-            console.log("𝐄𝐱𝐢𝐬𝐭𝐢𝐧𝐠 𝐦𝐞𝐦𝐛𝐞𝐫: " + member.user.tag + " already in stats.json with a level of " + stats[member.user.id].level + ".");
-        }
-        else {
+        if (stats[member.user.id] === undefined) {
             stats[member.user.id] = {
                 "xp": 0,
                 "level": 0,
                 "xpRequired": 100,
                 "joinTime": 0,
-                "leaveTime": 0
+                "leaveTime": 0,
+                "user": member.user.id
             }
             console.log("𝐍𝐞𝐰 𝐦𝐞𝐦𝐛𝐞𝐫: " + member.user.tag + " added to stats.json.");
+        }
+        else {
+            console.log("𝐄𝐱𝐢𝐬𝐭𝐢𝐧𝐠 𝐦𝐞𝐦𝐛𝐞𝐫: " + member.user.tag + " already in stats.json with a level of " + stats[member.user.id].level + ".");
         }
     }
 }
